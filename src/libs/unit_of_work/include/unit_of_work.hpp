79ae2190a0b1ce38d60b5ea7807a5c476e0287b1
SanSanchezzz <13lex04@gmail.com>
2020-04-26 03:17:22 +0300

    Unit of work added

---
+++ src/libs/unit_of_work/include/unit_of_work.hpp
@@ -0,0 +1,41 @@
+#ifndef UNIT_OF_WORK_HPP
+#define UNIT_OF_WORK_HPP
+
+#include <iostream>
+//#include <boost/property_tree/ptree.hpp>
+
+#include "unit_of_work_header.hpp"
+boost::property_tree::ptree heroTree;
+
+class UnitOfWork {
+public:
+    UnitOfWork() = default;
+    ~UnitOfWork() = default;
+    void foo() {std::cout << "Work";};
+    boost::property_tree::ptree  sing_up();
+    boost::property_tree::ptree get_index_tweet();
+    boost::property_tree::ptree login();
+    boost::property_tree::ptree authenticate();
+    boost::property_tree::ptree logout();
+    boost::property_tree::ptree user_update();
+    boost::property_tree::ptree get_user();
+    boost::property_tree::ptree get_profile();
+    boost::property_tree::ptree profile_update();
+    boost::property_tree::ptree get_like_tweets();
+    boost::property_tree::ptree vote();
+    boost::property_tree::ptree create_tweet();
+
+private:
+    vector<Profile> get_sub();
+    vector<Tag> get_tags();
+
+    ProfileRepository profile_repositrory;
+    TweetRepository  tweet_repository;
+    TagRepository tag_repository;
+    UserRepository user_repository;
+    VoteRepository vote_repository;
+    SubscriptionRepository subscription_repository;
+
+};
+
+#endif // UNIT_OF_WORK_HPP
