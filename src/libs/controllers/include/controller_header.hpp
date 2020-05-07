982172df9854c927470fb689c76e1f7d3b6ed92b
SanSanchezzz <13lex04@gmail.com>
2020-05-07 04:17:59 +0300

    Исправлено ужасное

--- src/libs/controllers/include/controller_header.hpp
+++ src/libs/controllers/include/controller_header.hpp
@@ -5,4 +5,18 @@
 #include <boost/property_tree/ptree.hpp>
 #include "unit_of_work.hpp"
 
+#include "create_tweet_controller.hpp"
+#include "index_controller.hpp"
+#include "login_controller.hpp"
+#include "logout_controller.hpp"
+#include "profile_controller.hpp"
+#include "signup_controller.hpp"
+#include "subscription_controller.hpp"
+#include "tags_controller.hpp"
+#include "tweet_like_controller.hpp"
+#include "update_profile_controller.hpp"
+#include "update_user_controller.hpp"
+#include "user_controller.hpp"
+#include "vote_controller.hpp"
+
 #endif
