20cfcc40f12c41820da5b8cdf7ddbf9d669a4080
SanSanchezzz <13lex04@gmail.com>
2020-05-13 15:36:45 +0300

    Исправлен конструктор для профиля

--- src/libs/models/include/profile.hpp
+++ src/libs/models/include/profile.hpp
@@ -7,7 +7,7 @@ class Profile {
 public:
     Profile() : profile_id(1), user_id(1), username("user"), avatar("avatar"), birthday("birthday") {}
 
-    Profile(int profile_id, int user_id, std::string _username, std::string _birthday, std::string _avatar):
+    Profile(int profile_id, int user_id, std::string& _username, std::string& _birthday, std::string& _avatar):
     profile_id(profile_id),
     user_id(user_id),
     username(std::move(_username)),
