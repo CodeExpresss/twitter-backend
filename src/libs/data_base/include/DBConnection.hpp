56116cfa53f5b3aded51938d708597df06b3a90d
GregoryBS <aanonimov117@gmail.com>
2020-05-13 18:01:12 +0300

    Fix link of db.

--- src/libs/data_base/include/DBConnection.hpp
+++ src/libs/data_base/include/DBConnection.hpp
@@ -11,7 +11,8 @@ class DBConnection
 {
 public:
     DBConnection(const string& host, const string& dbname, const string& user, 
-                 const string& password, int port) {
+                 const string& password, int port)
+    {
         connection.reset(PQsetdbLogin(host.c_str(), to_string(port).c_str(),
                                       nullptr, nullptr, dbname.c_str(), user.c_str(),
                                       password.c_str()), &PQfinish);
