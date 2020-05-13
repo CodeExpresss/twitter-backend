6d61b16ea51a0afe6ee926f22af3f276c5dab477
Sarafa2n <slaft3@yandex.ru>
2020-05-13 15:17:21 +0300

    Добавлены сериализаторы для полного юзера и одного твита

--- src/libs/serialize/include/serialize.hpp
+++ src/libs/serialize/include/serialize.hpp
@@ -74,6 +74,55 @@ template<> struct Serialize<std::vector<Profile>> {
     }
 };
 
+template<> struct Serialize<std::tuple<User, Profile>> {
+    boost::property_tree::ptree operator() (std::tuple<User, Profile> user) {
+        boost::property_tree::ptree tweet_tree,
+                child_item,
+                child_profile,
+                child_user;
+
+        Profile profile;
+        User system_user;
+
+        std::tie(system_user, profile) = user;
+        child_user.put("email", system_user.get_email());
+        child_profile.put("username", profile.get_username());
+//        child_profile.put("avatar", profile.get_avatar());
+        child_profile.put("birthday", profile.get_birthday());
+
+        child_item.add_child("system_user", child_user);
+        child_item.add_child("profile", child_profile);
+
+        tweet_tree.add_child("user", child_item);
+
+        return tweet_tree;
+    }
+};
+
+template<> struct Serialize<std::tuple<Tweet, Profile>> {
+    boost::property_tree::ptree operator() (std::tuple<Tweet, Profile> new_tweet) {
+        boost::property_tree::ptree tweet_tree,
+                child_item,
+                child_tweet,
+                child_user;
+
+        Profile profile;
+        Tweet tweet;
+
+        std::tie(tweet, profile) = new_tweet;
+        child_tweet.put("text", tweet.get_text());
+        child_tweet.put("data", tweet.get_date());
+        child_user.put("username", profile.get_username());
+
+        child_item.add_child("tweet", child_tweet);
+        child_item.add_child("author", child_user);
+
+        tweet_tree.add_child("wall_item", child_item);
+
+        return tweet_tree;
+    }
+};
+
 
 template<> struct Serialize<std::vector<std::tuple<Tweet, Profile>>> {
     boost::property_tree::ptree operator() (std::vector<std::tuple<Tweet, Profile>> wall) {
