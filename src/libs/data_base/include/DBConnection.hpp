26c398f465a9e58a1747f6a31476ceb224c84ea9
Sarafa2n <slaft3@yandex.ru>
2020-05-13 11:52:09 +0300

    Отладка сборки проекта

--- src/libs/data_base/include/DBConnection.hpp
+++ src/libs/data_base/include/DBConnection.hpp
@@ -11,8 +11,18 @@ class DBConnection
 {
 public:
     DBConnection(const string& host, const string& dbname, const string& user, 
-                 const string& password, int port);
-    ~DBConnection();
+                 const string& password, int port) {
+        connection.reset(PQsetdbLogin(host.c_str(), to_string(port).c_str(),
+                                      nullptr, nullptr, dbname.c_str(), user.c_str(),
+                                      password.c_str()), &PQfinish);
+
+        if (PQstatus(connection.get()) != CONNECTION_OK && PQsetnonblocking(connection.get(), 1) != 0 )
+        {
+            throw runtime_error(PQerrorMessage(connection.get()));
+        }
+    }
+
+    ~DBConnection() = default;
     
     shared_ptr<PGconn> get_connection() const;
 
