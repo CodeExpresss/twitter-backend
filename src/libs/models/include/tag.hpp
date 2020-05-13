4cb1a1407cb9faf26878ec9fe1b7f655cfd8cb11
SanSanchezzz <13lex04@gmail.com>
2020-05-13 15:35:01 +0300

    Добавлен конструктор для тега

--- src/libs/models/include/tag.hpp
+++ src/libs/models/include/tag.hpp
@@ -5,7 +5,7 @@
 
 class Tag {
 public:
-    Tag() = default;
+    Tag(int tag_id, std::string &title) : tag_id(tag_id), title(title) {}
     ~Tag() = default;
 
     int get_tag_id() {return tag_id;}
