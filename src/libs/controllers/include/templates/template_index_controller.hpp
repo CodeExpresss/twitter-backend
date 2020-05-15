43a5425dae04e93943880ea1da5780a4c41d1ec4
Sarafa2n <slaft3@yandex.ru>
2020-05-16 00:10:20 +0300

    Исправление обработки ошибочного запроса

--- src/libs/controllers/include/templates/template_index_controller.hpp
+++ src/libs/controllers/include/templates/template_index_controller.hpp
@@ -2,10 +2,13 @@
 
 template <class Serialize>
 boost::property_tree::ptree IndexController<Serialize>::get_queryset(int profile_id) {
+    boost::property_tree::ptree default_tree;
+    default_tree.put("status", "error");
+
     wall = worker->get_index_tweet(profile_id);
     if(wall.size() > 0)
         return serialize(wall);
     else {
-        return static_cast<boost::property_tree::ptree>(nullptr);
+        return default_tree;
     }
 };
--- src/libs/controllers/include/templates/template_tweet_like_controller.hpp
+++ src/libs/controllers/include/templates/template_tweet_like_controller.hpp
@@ -2,11 +2,13 @@
 
 template <class Serialize>
 boost::property_tree::ptree TweetLikeController<Serialize>::get_queryset(int profile_id) {
+    boost::property_tree::ptree default_tree;
+    default_tree.put("status", "error");
     like_wall = worker->get_like_tweets(profile_id);
 
     if( like_wall.size() > 0)
         return serialize(like_wall);
     else {
-        return static_cast<boost::property_tree::ptree>(nullptr);
+        return default_tree;
     }
 };
