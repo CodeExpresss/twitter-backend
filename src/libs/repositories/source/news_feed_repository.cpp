596eec7a55e8e4721b568af4af01a652b1d95282
SanSanchezzz <13lex04@gmail.com>
2020-05-17 01:51:59 +0300

    Добавлен метод создания ленты для новых пользователей

--- src/libs/repositories/source/news_feed_repository.cpp
+++ src/libs/repositories/source/news_feed_repository.cpp
@@ -31,12 +31,31 @@ std::vector<std::pair<Tweet, Profile>> NewsFeedRepository::get_by_id(int id, err
     return result;
 }
 
+void NewsFeedRepository::create(Profile& item, err_code& rc) {
+    std::vector<std::vector<std::string>> query_result = {};
+
+    std::string query =
+            (boost::format("insert into newsfeed values(%1%, %2%, null);")
+             % item.get_profile_id()
+             % item.get_user_id()
+             ).str();
+
+    if (auto ctrl = db_controller.lock()) {
+        if (ctrl->run_query(query, query_result)) {
+            rc = OK;
+        } else
+            rc = NOT_EXIST;
+    } else
+        rc = NO_CTRL;
+
+}
+
 void NewsFeedRepository::update(std::pair<Tweet, Profile> &item, int profile_id, err_code &rc) {
     std::vector<std::vector<std::string>> query_result = {};
 
     std::string query =
             (boost::format("update newsfeed set news = news || "
-                           "row(%1%, '%2%', '%3%', %4%, %5%, '%6%', %7%, '%8%')::content where id = %9%")
+                           "row(%1%, '%2%', '%3%', %4%, %5%, '%6%', %7%, '%8%')::content where id = %9%;")
              % item.first.get_tweet_id()
              % item.first.get_text()
              % item.first.get_date()
