d9ec05efcbe839a1fdfa54dea83a796e3c2ff3db
SanSanchezzz <13lex04@gmail.com>
2020-05-16 20:53:43 +0300

    Разделены методы получения подписчиков и подписок

--- src/libs/repositories/include/subscription_repository.hpp
+++ src/libs/repositories/include/subscription_repository.hpp
@@ -12,7 +12,8 @@ public:
 
     std::vector<Subscription> get_where() {}
     bool check_subscription(Subscription& item, err_code& rc);
-    std::vector<int> get_by_id(int id, err_code &rc);
+    std::vector<int> get_by_invitee_id(int id, err_code &rc);
+    std::vector<int> get_by_inviter_id(int id, err_code &rc);
     void create(Subscription &item, err_code &rc);
     void update(Subscription &item, err_code &rc);
     void erase(int id, err_code &rc);
--- src/libs/repositories/source/subscription_repository.cpp
+++ src/libs/repositories/source/subscription_repository.cpp
@@ -1,9 +1,32 @@
-#include "../include/subscription_repository.hpp" 
+#include "../include/subscription_repository.hpp"
 
-std::vector<int> SubscriptionRepository::get_by_id(int id, err_code &rc) 
-{
+std::vector<int> SubscriptionRepository::get_by_inviter_id(int id, err_code &rc) {
 	std::vector<std::vector<std::string>> query_result = {};
-    std::string query = 
+    std::string query =
+        (boost::format("select invitee_id, is_active from subscription where inviter_id = %1%;") % id).str();
+    std::vector<int> result = {};
+	if (auto ctrl = db_controller.lock()) {
+		if (ctrl->run_query(query, query_result)) {
+            for (int i = 0; i < query_result.size(); i++) {
+                if (!query_result[i][1].compare("t"))
+                    result.push_back(std::stoi(query_result[i][0]));
+            }
+			rc = OK;
+		}
+		else {
+			rc = NOT_EXIST;
+        }
+	}
+	else {
+		rc = NO_CTRL;
+    }
+
+	return result;
+}
+
+std::vector<int> SubscriptionRepository::get_by_invitee_id(int id, err_code &rc) {
+	std::vector<std::vector<std::string>> query_result = {};
+    std::string query =
         (boost::format("select inviter_id, is_active from subscription where invitee_id = %1%;") % id).str();
     std::vector<int> result = {};
 	if (auto ctrl = db_controller.lock())
@@ -30,7 +53,8 @@ bool SubscriptionRepository::check_subscription(Subscription& item, err_code& rc
     int inviter_id = item.get_inviter_id();
     int invitee_id = item.get_invitee_id();
     std::string query =
-        (boost::format("select is_active from subscription where inviter_id = %1%, invitee_id = %2%;") % inviter_id % invitee_id).str();
+        (boost::format("select is_active from subscription where inviter_id = %1% and invitee_id = %2%;")
+         % inviter_id % invitee_id).str();
     bool result = false;
 	if (auto ctrl = db_controller.lock()) {
 		if (ctrl->run_query(query, query_result)) {
