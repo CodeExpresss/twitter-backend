2b65f771ba914337a3f414651e26f99d3175911f
SanSanchezzz <13lex04@gmail.com>
2020-05-17 01:50:44 +0300

    Исправлен метод вызова uow

--- src/libs/controllers/include/templates/template_follow_controllers.hpp
+++ src/libs/controllers/include/templates/template_follow_controllers.hpp
@@ -2,5 +2,5 @@
 
 template <class Serialize>
 boost::property_tree::ptree FollowController<Serialize>::get_queryset() {
-    return serialize(worker->following(subscriptions.get_inviter_id(), subscriptions.get_invitee_id()));
+    return serialize(worker->following(subscriptions));
 }
