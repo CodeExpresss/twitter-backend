f6e225880ef941c4548447c053e880615fbf2a64
SanSanchezzz <13lex04@gmail.com>
2020-05-17 01:53:23 +0300

    Добавлен метод проверки существования профиля

--- src/libs/repositories/include/profile_repository.hpp
+++ src/libs/repositories/include/profile_repository.hpp
@@ -9,10 +9,11 @@ public:
     ProfileRepository(std::weak_ptr<DBController<DBConnection>> controller) : db_controller(controller) {}
     ~ProfileRepository() = default;
 
+    bool check_profile_username(Profile& item, err_code& rc);
     std::vector<Profile> get_where() {}
-	
+
     Profile get_by_id(int id, err_code &rc);
-	
+
     void create(Profile &item, err_code &rc);
     void update(Profile &item, err_code &rc);
     void erase(int id, err_code &rc);
