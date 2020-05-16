cb2fd6416ee52b99d2906db54f39f106963e48b2
SanSanchezzz <13lex04@gmail.com>
2020-05-17 02:12:48 +0300

    Добавлена реализация методов, связанных с newsfeed

--- src/libs/unit_of_work/source/unit_of_work.cpp
+++ src/libs/unit_of_work/source/unit_of_work.cpp
@@ -1,7 +1,42 @@
 #include "unit_of_work_header.hpp"
 #include "unit_of_work.hpp"
 #include "repositories_header.hpp"
-#include "subscription_repository.hpp"
+
+std::pair<unsigned short int, std::string> UnitOfWork::sing_up(User user, Profile profile) {
+    err_code rc;
+    bool status = user_repository->check_user_email(user, rc);
+    if (rc == NOT_EXIST) {
+        user_repository->create(user, rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
+    }
+    else if (rc == DELETED) {
+        user_repository->erase(user.get_user_id(), rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
+    }
+    else if (rc == OK && status == true) {
+        return std::pair<unsigned short, std::string>(404, "USER ALREADY_EXIST");
+    }
+    else {
+        return std::pair<unsigned short, std::string>(404, "DB err");
+    }
+
+    //profile_repositrory->check_profile_username(profile, rc);
+    profile_repositrory->create(profile, rc);
+    if (rc != OK) {
+        return std::pair<unsigned short, std::string>(404, "err");
+    }
+
+    news_feed_repository->create(profile, rc);
+    if (rc != OK) {
+        return std::pair<unsigned short, std::string>(404, "err");
+    }
+
+    return std::pair<unsigned short, std::string>(200, "Ok");
+}
 
 std::vector<Profile> UnitOfWork::get_subscriptions(int profile_id, err_code& rc) {
     std::vector<int> profiles_id = subscription_repository->get_by_invitee_id(profile_id, rc);
@@ -22,7 +57,6 @@ std::pair<unsigned short int, std::string> UnitOfWork::create_tweet(Tweet tweet)
     if (rc != OK) {
         return std::pair<unsigned short, std::string>(404, "err");
     }
-    //if (tags.size)
 
     for (auto tag: tags) {
         tag_repository->create(tag, rc);
@@ -31,45 +65,78 @@ std::pair<unsigned short int, std::string> UnitOfWork::create_tweet(Tweet tweet)
         }
     }
 
+    std::pair<Tweet, Profile> updates;
+    updates.first = tweet;
+    updates.second = profile_repositrory->get_by_id(tweet.get_profile_id(), rc);
+
+    auto subs = subscription_repository->get_by_inviter_id(tweet.get_profile_id(), rc);
+    for (int i : subs) {
+        news_feed_repository->update(updates, i, rc);
+    }
+
     return std::pair<unsigned short, std::string>(200, "Ok");
 }
 
 std::pair<unsigned short int, std::string> UnitOfWork::following(Subscription subscription) {
     err_code rc;
 
-    //bool status = subscription_repository->check_subscription(subscription, rc);
-    //if (rc == NOT_EXIST) {
-        //subscription.set_is_active(true);
-        //subscription_repository->create(subscription, rc);
-        //if (rc != OK) {
-            //return std::pair<unsigned short, std::string>(404, "err");
-        //}
-    //}
-    //else if (rc == OK) {
-        //subscription.set_is_active(!status);
-        //subscription_repository->update(subscription, rc);
-    /*}*/
+    bool status = subscription_repository->check_subscription(subscription, rc);
+    if (rc == NOT_EXIST) {
+        subscription.set_is_active(true);
+        subscription_repository->create(subscription, rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
 
+        std::vector<Tweet> tweets;
+        tweets = tweet_repository->get_by_profile_id(subscription.get_inviter_id(), rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
 
-    //auto subs = subscription_repository->get_by_id(tweet.get_profile_id(), rc);
-    //здесь вставить функцию получения подписчиков
-    std::vector<int> subs;
-    subs.push_back(2);
-    std::pair<Tweet, Profile> updates;
-    updates.first = tweet;
-    updates.second = profile_repositrory->get_by_id(tweet.get_profile_id(), rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
 
-    for (int i : subs) {
-        news_feed_repository->update(updates, i, rc);
-      
-    subscription_repository->create(subscription, rc);
-    if (rc != OK) {
+        auto invitee = subscription.get_invitee_id();
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
+
+        std::pair<Tweet, Profile> updates;
+        updates.second = profile_repositrory->get_by_id(subscription.get_inviter_id(), rc);
+        for (auto tweet: tweets) {
+            updates.first = tweet;
+            news_feed_repository->update(updates, invitee, rc);
+            if (rc != OK) {
+                return std::pair<unsigned short, std::string>(404, "err");
+            }
+        }
+    }
+    else if (rc == OK || rc == DELETED) {
+        subscription.set_is_active(!status);
+        subscription_repository->update(subscription, rc);
+        if (rc != OK) {
+            return std::pair<unsigned short, std::string>(404, "err");
+        }
+    }
+    else {
         return std::pair<unsigned short, std::string>(404, "err");
-
     }
 
     return std::pair<unsigned short, std::string>(200, "Ok");
 }
+
+
+std::vector<std::pair<Tweet, Profile>> UnitOfWork::get_index_tweet(int profile_id) {
+    err_code rc;
+    std::vector<std::pair<Tweet, Profile>> contents = news_feed_repository->get_by_id(profile_id, rc);
+
+    return contents;
+}
+
+
+
 /*Profile UnitOfWork::get_profile(int profile_id) {*/
     //Profile _profile;
     //return _profile;
