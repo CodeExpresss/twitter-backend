58fa5fc785a7b63fbccb9180a63ff3f9ec73c2ac
NikitaLobaev <nikitos2255@yandex.ru>
2020-05-27 20:26:30 +0300

    Удалил ненужные папки (build, debug...).

--- cmake-build-debug/src/libs/unit_of_work/CMakeFiles/unit_of_work.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libunit_of_work.a"
-)
--- mybuild/src/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Desktop/МГТУ/Технопарк/Программирование на C и CPP/twitter-backend/src/test
-# Build directory: /home/nick/Desktop/МГТУ/Технопарк/Программирование на C и CPP/twitter-backend/mybuild/src/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtest "/tests_gtest")
-set_tests_properties(gtest PROPERTIES  _BACKTRACE_TRIPLES "/home/nick/Desktop/МГТУ/Технопарк/Программирование на C и CPP/twitter-backend/src/test/CMakeLists.txt;25;add_test;/home/nick/Desktop/МГТУ/Технопарк/Программирование на C и CPP/twitter-backend/src/test/CMakeLists.txt;0;")
