0dbb8e424ea649e25fdd300e2d4dd4f2a630b036
GregoryBS <aanonimov117@gmail.com>
2020-06-02 16:30:28 +0300

    Fixing codestyle in db and repositories.

--- src/libs/repositories/include/subscription_repository.hpp
+++ src/libs/repositories/include/subscription_repository.hpp
@@ -5,21 +5,20 @@
 #include "subscription.hpp"
 
 class SubscriptionRepository {
-public:
-    SubscriptionRepository(std::weak_ptr<DBController<DBConnection>> controller) : db_controller(controller) {};
-    ~SubscriptionRepository() = default;
+ public:
+  SubscriptionRepository(std::weak_ptr<DBController<DBConnection>> controller)
+      : db_controller(controller){};
+  ~SubscriptionRepository() = default;
 
+  bool check_subscription(Subscription &item, err_code &rc);
+  std::vector<int> get_by_invitee_id(int id, err_code &rc);
+  std::vector<int> get_by_inviter_id(int id, err_code &rc);
+  void create(Subscription &item, err_code &rc);
+  void update(Subscription &item, err_code &rc);
+  void erase(int id, err_code &rc);
 
-    std::vector<Subscription> get_where() {}
-    bool check_subscription(Subscription& item, err_code& rc);
-    std::vector<int> get_by_invitee_id(int id, err_code &rc);
-    std::vector<int> get_by_inviter_id(int id, err_code &rc);
-    void create(Subscription &item, err_code &rc);
-    void update(Subscription &item, err_code &rc);
-    void erase(int id, err_code &rc);
-
-private:
-    std::weak_ptr<DBController<DBConnection>> db_controller;
+ private:
+  std::weak_ptr<DBController<DBConnection>> db_controller;
 };
 
-#endif // SUBSCRIPTION_REPOSITORY_HPP
+#endif  // SUBSCRIPTION_REPOSITORY_HPP
